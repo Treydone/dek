3de935bd19414d3d4937d3272502a1685e50501b
treydone <vdevillers76@gmail.com>
2018-11-06 11:04:45 +0100

    Fix tests

--- plugins/ambari-cluster-resolver/src/test/java/fr/layer4/hhsl/AmbariClusterInfoResolverTest.java
+++ plugins/ambari-cluster-resolver/src/test/java/fr/layer4/hhsl/AmbariClusterInfoResolverTest.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -174,10 +174,11 @@ public class AmbariClusterInfoResolverTest {
         // Then
         assertThat(configurationFiles).hasSize(1).containsOnlyKeys("HIVE");
         assertThat(configurationFiles.get("HIVE")).hasSize(1).containsOnlyKeys("test.xml");
-        String expected = new String(IOUtils.toByteArray(AmbariClusterInfoResolver.class.getClassLoader().getResourceAsStream("test.xml")));
-        String actual = new String(configurationFiles.get("HIVE").get("test.xml"));
+
+        byte[] expected = IOUtils.toByteArray(AmbariClusterInfoResolver.class.getClassLoader().getResourceAsStream("test.xml"));
+        byte[] actual = configurationFiles.get("HIVE").get("test.xml");
         assertThat(actual)
-                .withFailMessage("Got response: " + actual + ", was expecting: " + expected)
+                .withFailMessage("Got response: " + new String(actual) + ", was expecting: " + new String(expected))
                 .isEqualTo(expected);
     }
 }
