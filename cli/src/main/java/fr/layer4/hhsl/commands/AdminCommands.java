13c111546495317b92203a6c0a83f69634429a66
treydone <vdevillers76@gmail.com>
2018-11-07 18:12:46 +0100

    Update commands

--- cli/src/main/java/fr/layer4/hhsl/commands/AdminCommands.java
+++ cli/src/main/java/fr/layer4/hhsl/commands/AdminCommands.java
@@ -12,10 +12,10 @@ package fr.layer4.hhsl.commands;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,6 +26,7 @@ package fr.layer4.hhsl.commands;
  * #L%
  */
 
+import fr.layer4.hhsl.Constants;
 import fr.layer4.hhsl.store.LockableStore;
 import fr.layer4.hhsl.store.Store;
 import lombok.extern.slf4j.Slf4j;
@@ -33,6 +34,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Slf4j
 @ShellComponent
@@ -57,12 +63,25 @@ public class AdminCommands {
                 : Availability.unavailable("Local database is already ready, use clean before re-init");
     }
 
-    @ShellMethod(key = "clean", value = "Clean local database", group = "Configuration")
-    public void clean() {
+    @ShellMethod(key = "clean", value = "Clean all", group = "Configuration")
+    public void clean() throws IOException {
+        cleanStore();
+        cleanArchives();
+    }
+
+    @ShellMethod(key = "clean store", value = "Clean local database", group = "Configuration")
+    public void cleanStore() {
         store.purge();
     }
 
-    public Availability cleanAvailability() {
+    @ShellMethod(key = "clean archive", value = "Clean archives", group = "Configuration")
+    public void cleanArchives() throws IOException {
+        String basePath = Constants.getRootPath();
+        String archivesPath = basePath + File.separator + Constants.ARCHIVES;
+        Files.deleteIfExists(Paths.get(archivesPath));
+    }
+
+    public Availability cleanStoreAvailability() {
         return store.isReady()
                 ? Availability.available()
                 : Availability.unavailable("Local database is not ready");
